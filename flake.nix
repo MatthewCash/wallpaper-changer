5127b3492e304d11caec957c7408cb20c5e38622
Matthew_Cash <matthew@matthew-cash.com>
2022-09-06 13:53:11 -0700

    Cleanup flake inputs

--- flake.nix
+++ flake.nix
@@ -3,11 +3,7 @@
 
     inputs = {
         nixpkgs.url = "nixpkgs/nixos-unstable";
-
-        flake-utils = {
-            url = "github:numtide/flake-utils";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+        flake-utils.url = "github:numtide/flake-utils";
     };
 
     outputs = inputs @ { self, nixpkgs, flake-utils, ... }:
